c9a562cc3125a973a3a99a3f75f1549bfb320081
ssionii <ssionii353@gmail.com>
2020-10-16 21:25:59 +0900

    [SB-30] Problem repository 만들고 예시 문제 올리기

---
+++ xss/problem1.java
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg949\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\fnil\fcharset129 AppleSDGothicNeo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab560
+\pard\pardeftab560\ri-340\sl270\qj\partightenfactor0
+
+\f0\fs22 \cf2 <% String keyword = request.getParameter("keyword"); %>\
+\pard\pardeftab560\ri-340\qj\partightenfactor0
+
+\f1 \cf2 \'b0\'cb\'bb\'f6\'be\'ee
+\f0  : <%=keyword%>}
