34688b55da106913abff4d3f1c4d2c52ef688a30
ssionii <ssionii353@gmail.com>
2020-11-02 14:55:06 +0900

    sh 경로 설정

--- create_container.sh
+++ create_container.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 사용자 폴더 생성
-cd /Users/ssionii/Desktop/secubook/user/userCode
+# $1: 사용자 ID
+cd /home/ubuntu/secubook/user/userCode
 mkdir $1
 
 # 컨테이너 생성
@@ -12,9 +13,8 @@ docker create \
 -e TZ=Asia/Seoul \
 -e PASSWORD=0000 \
 -e SUDO_PASSWORD=0000 \
--p $2:8443 \
--v /Users/ssionii/Desktop/secubook/user/userCode/$1:/config \
--v /Users/ssionii/Desktop/secubook/log/score:/opt/$1/log/score \
+-v /home/ubuntu/secubook/user/userCode/$1:/config \
+-v /home/ubuntu/secubook/log/score:/opt/$1/log/score \
 secubook
 
 # 컨테이너 실행
@@ -24,12 +24,10 @@ docker start $1
 docker stop $1
 
 # 문제 복사
-cd /Users/ssionii/Desktop/secubook_problem
+cd /home/ubuntu/secubook_problem
 cp -r problems problems_cp
-mv problems_cp /Users/ssionii/Desktop/secubook/user/userCode/$1/workspace
-cd /Users/ssionii/Desktop/secubook/user/userCode/$1/workspace
+mv problems_cp /home/ubuntu/secubook/user/userCode/$1/workspace
+cd /home/ubuntu/secubook/user/userCode/$1/workspace
 mv problems_cp problems
 
 # CodeAnalyzer 다시 생성
-docker start $1
-docker exec $1 cd CodeAnalyzer/src && javac com/company/Main.java
--- score_code.sh
+++ score_code.sh
@@ -2,5 +2,5 @@
 
 # 사용자 컨테이너 실행
 # $1: 사용자 ID, $2: 문제 번호
-cd /Users/ssionii/Desktop/secubook_problem
+cd /home/ubuntu/secubook_problem
 docker exec $1 ./run_codeanalyzer.sh $1 $2
